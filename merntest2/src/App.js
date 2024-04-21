61c018220c6e7b327b349e0696486f49168c8b17
HarshPatiL11 <patilharsh497@gmail.com>
2024-04-21 14:42:26 +0530

    second commit

--- merntest2/src/App.js
+++ merntest2/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home'
+import { Route, Routes } from 'react-router-dom';
+import About from './Components/About';
+import GetAxios from './Components/Getaxios';
+import PostAxios from './Components/PostAxios';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+     
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/showdata' element={<GetAxios/>}/>
+        <Route path='/update' element={<PostAxios/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ merntest2/src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Img1 from '../Assets/img1.jpeg'
+
+const About = () => {
+  return (
+    <div>
+       
+      <div className='AboutmainDiv'>
+        <div className=''>
+          <img src={Img1} alt=''/>
+        </div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla accusamus dolore sint sit ullam incidunt vero tempora dicta rerum eveniet officia similique, natus eos veniam nemo enim ex voluptates et.</div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default About
